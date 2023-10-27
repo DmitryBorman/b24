5c977da3277a14c5f7162178f39f76b718f7a49c
DmitryBorman <pirsing@list.ru>
2023-10-27 08:45:48 +0300

    Create checking_phone_and_email_duplicates.php
    checking phone and email duplicates for the business process designer

---
+++ bp_designer/checking_phone_and_email_duplicates.php
@@ -0,0 +1,22 @@
+<?php
+$rootActivity = $this->GetRootActivity();
+$value = $rootActivity->GetVariable("Incoming_request");
+
+if(!strpos($value, '@')) {
+    $q = substr($value, 0, 1);
+    if (in_array($q, ['8', '7', '+'])) {
+        $phoneNumber = preg_replace('/\s|\+|-|\(|\)/', '', $value);
+        if (is_numeric($phoneNumber)) {
+            $value = substr($value, -10);
+        }
+    }
+}
+
+\CModule::includeModule("crm");
+$res = \CCrmFieldMulti::GetList(['ID'=>'asc'],['ENTITY_ID' =>'CONTACT','%VALUE' => $value]);
+while($arEl = $res->fetch()){
+    $outgoing_response = $arEl['ELEMENT_ID'];
+    break;
+};
+
+$rootActivity->SetVariable("Outgoing_response",$outgoing_response);
